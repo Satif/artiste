d6661bee208e0ac5cc821b02d714fe821c5c784e
olegdemkovych <oleg.demkovych@temy.co>
2017-07-12 17:09:22 +0300

    Fiux

--- gulp/conf.js
+++ gulp/conf.js
@@ -11,7 +11,7 @@ exports.paths = {
 };
 
 exports.url = {
-  database: 'mongodb://artside:art9874RT#@ds157112.mlab.com:57112/heroku_20v3s87z'
+  database: 'mongodb://admin:art9874RT#@ds157112.mlab.com:57112/heroku_20v3s87z'
   // database: 'mongodb://artside:art9874RT#@cluster0-shard-00-00-uxde6.mongodb.net:27017,cluster0-shard-00-01-uxde6.mongodb.net:27017,cluster0-shard-00-02-uxde6.mongodb.net:27017/artside?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
   // database: 'mongodb://localhost/artside'
 };
